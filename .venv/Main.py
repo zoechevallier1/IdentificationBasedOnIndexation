63ae6de223fe646e45730ce928dc67fc07fe977b
Zoé Chevallier <zo.chevallier@uvsq.fr>
2025-01-27 10:26:54 +0100

    Add the main file

---
+++ .venv/Main.py
@@ -0,0 +1,9 @@
+import Files.SourceFile as sourceFile
+import Files.TargetFile as targetFile
+
+import Files.GraphView as GraphView
+
+
+if __name__ == "__main__":
+    source = sourceFile.SourceFile("C:/Users/z.chevallier/Documents/Développement/Indexation/source.nq")
+    target = targetFile.TargetFile("C:/Users/z.chevallier/Documents/Développement/Indexation/targetSchema.nq")
